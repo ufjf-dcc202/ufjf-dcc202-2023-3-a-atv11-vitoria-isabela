21bd0fdf4b80dfbd1460205131369e5fa093a390
vitoria-isabela <VitoriaIsabela.deOliveira@thomsonreuters.com>
2023-12-05 21:03:34 -0300

    add limparElementosDaLista

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ const olSaida = document.querySelector('#itens');
 const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
+btnLimpar.addEventListener('click', limparElementosDaLista);
+btnAdicionar.addEventListener('click', criaElementoNaLista);
+
+atualizarItensDeLista();
+
 function criaElementoNaLista(){
-    const texto  = pEntrada.textContent;
-    adicionaNaLista(texto);
-    atualizarItensDeLista();
-    pEntrada.textContent="";
-    pEntrada.focus();
-  }
+  const texto  = pEntrada.textContent;
+  adicionaNaLista(texto);
+  atualizarItensDeLista();
+  pEntrada.textContent="";
+  pEntrada.focus();
+}
+
+function limparElementosDaLista(){
+  limpaLista();
+  atualizarItensDeLista();
+}
