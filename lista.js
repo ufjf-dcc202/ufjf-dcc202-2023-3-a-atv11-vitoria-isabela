3c48c2e3359378cc38b26b17aaa8e41ae2717bc8
vitoria-isabela <VitoriaIsabela.deOliveira@thomsonreuters.com>
2023-12-05 21:02:10 -0300

    add lista

---
+++ lista.js
@@ -0,0 +1,17 @@
+const lista = ["Um", "Dois", "Três"];
+
+export function getLista(){
+  return lista;
+}
+
+export function limpaLista(){
+  lista.splice(0);
+}
+
+export function adicionaNaLista(novoItem){
+  lista.push(novoItem);
+}
+
+export function removeDaLista(posicao){
+  lista.splice(posicao, posicao);
+}
