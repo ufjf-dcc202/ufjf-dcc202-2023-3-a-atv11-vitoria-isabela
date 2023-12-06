a9bbfc82b9bcc888ae245dbe591b40685849c96a
vitoria-isabela <VitoriaIsabela.deOliveira@thomsonreuters.com>
2023-12-05 21:02:50 -0300

    add criarElementoLista

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ const pEntrada = document.querySelector('#entrada');
 const olSaida = document.querySelector('#itens');
 const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
+
+function criaElementoNaLista(){
+    const texto  = pEntrada.textContent;
+    adicionaNaLista(texto);
+    atualizarItensDeLista();
+    pEntrada.textContent="";
+    pEntrada.focus();
+  }
