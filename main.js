5f6151aa92f0a837ae50b2a9cb860881682ae3b6
vitoria-isabela <VitoriaIsabela.deOliveira@thomsonreuters.com>
2023-12-05 21:01:43 -0300

    add passo 1

---
+++ main.js
@@ -0,0 +1,4 @@
+const pEntrada = document.querySelector('#entrada');
+const olSaida = document.querySelector('#itens');
+const btnAdicionar = document.querySelector('#adicionar');
+const btnLimpar = document.querySelector('#limpar');
